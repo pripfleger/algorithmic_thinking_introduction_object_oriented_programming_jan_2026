3231f5eaf0c40de044678e8a800caeda709fed9f
Priscylla Pfleger <cyllapfleger@hotmail.com>
2026-02-01 22:59:10 -0300

    Atualizado com tratamento de erros, conferir

--- imobiliaria.py
+++ imobiliaria.py
@@ -1,66 +1,87 @@
-contrato = 2000
-
-def quartos():
-    if opcao_moradia == "apartamento" and qtdade_quartos == "1":
-        orcamento = 700
-    elif opcao_moradia == "apartamento" and qtdade_quartos == "2":
-        orcamento = 700+200
-    elif opcao_moradia == "casa" and qtdade_quartos == "1":
-        orcamento = 900
-    elif opcao_moradia == "casa" and qtdade_quartos == "2":
-        orcamento = 900+250
-    else:
-        print("Erro no processamento, escolha novamente a quantidade de quartos!")
-
-def garagem():
-    if garagem == "N":
-        orcamento += 0
-    elif garagem == "S":
-        orcamento += 300
-    else:
-        print("Erro no processamento, você deseja uma vaga de garagem? (S)sim ou (N)não ")
-
-def garagem_estudio():
-    if garagem == "S":
-        orcamento += 250
-        vaga_extra = int(input("Quantas vagas extras? "))
-        if vaga_extra != 0:
-            orcamento += vaga_extra * 60
+def quartos(opcao_moradia, qtdade_quartos):
+    try:
+        if opcao_moradia == "apartamento" and qtdade_quartos == "1":
+            return  700
+        elif opcao_moradia == "apartamento" and qtdade_quartos == "2":
+            return 700 + 200
+        elif opcao_moradia == "casa" and qtdade_quartos == "1":
+            return 900
+        elif opcao_moradia == "casa" and qtdade_quartos == "2":
+            return 900 + 250
         else:
-            print("Erro no processamento, digite novamente a quantidade de vagas desejadas: ")
-        
-    elif garagem == "N":
-        orcamento += 200
-    else:
-        print("Não identificamos sua escolha, informe sua escolha novamente!")
+            print("Erro no processamento, escolha novamente a quantidade de quartos!")
+    except:
+        print("Quantidade de quartos não identificada! Tente novamente!")
 
-def criança():
-    criança = input("Você tem criança(s)? (S) sim ou (N) não ").upper()
-    if criança == "N":
+def garagem_geral(orcamento, garagem):
+    try:
+        if garagem == "N":
+            return orcamento
+        elif garagem == "S":
+            return orcamento + 300
+        else:
+            print("Erro no processamento, você deseja uma vaga de garagem? (S)sim ou (N)não ")
+    except:
+        print("Opção de garagem não identificada! Tente novamente!")
 
-    elif criança == "S":
+def garagem_estudio(orcamento, garagem):
+    try:
+        if garagem == "S":
+            orcamento += 250
+            vaga_extra = int(input("Quantas vagas extras? "))
+            if vaga_extra > 0:
+                return orcamento + (vaga_extra * 60)
+            else:
+                print("Erro no processamento, digite novamente a quantidade de vagas desejadas: ")
+            
+        elif garagem == "N":
+            return orcamento + 200
+        else:
+            print("Não identificamos sua escolha, informe sua escolha novamente!")
+            return orcamento
+    except:
+        print("Opção de garagem do estúdio não identificada! Tente novamente!")
+
+def desconto_crianca(orcamento):
+    crianca = input("Você tem criança(s)? (S) sim ou (N) não ").upper()
+    
+    try:
+        if crianca == "N":
+            return orcamento * 0.95
+        elif crianca == "S":
+            return orcamento
+        else:
+            print("Erro no processamento, digite novamente!")
+    except:
+        print("Informação de crianças não identificada! Tente novamente!")
 
 def valor_orcamento():
     orcamento = 0
     opcao_moradia = input("Você quer apartamento, casa ou estudio? ")
-            
-    if opcao_moradia == "apartamento" or opcao_moradia == "casa":
-        qtdade_quartos = input("Digite a quantidade de quartos desejada (1 ou 2): ")
-        garagem = input("Você deseja uma vaga de garagem? (S)sim ou (N)não ").upper()
-        quartos()
-        garagem()
 
-    elif opcao_moradia == "estudio":
-        orcamento = 1200
-        garagem = input("Você deseja uma vaga de garagem? (S)sim ou (N)não ").upper()
-        garagem_estudio()
+    try:        
+        if opcao_moradia == "apartamento" or opcao_moradia == "casa":
+            qtdade_quartos = input("Digite a quantidade de quartos desejada (1 ou 2): ")
+            garagem = input("Você deseja uma vaga de garagem? (S)sim ou (N)não ").upper()
+            orcamento = quartos(opcao_moradia, qtdade_quartos)
+            orcamento = garagem_geral(orcamento, garagem)
+            orcamento = desconto_crianca(orcamento)
+            print(f"Foi escolhido {opcao_moradia} e o orçamento é de R${orcamento:.2f}.")
 
-    else:
-        print("Opção não encontrada, escolha novamente!")
+        elif opcao_moradia == "estudio":
+            orcamento = 1200
+            garagem = input("Você deseja uma vaga de garagem? (S)sim ou (N)não ").upper()
+            orcamento = garagem_estudio(orcamento, garagem)
+            orcamento = desconto_crianca(orcamento)
+            print(f"Foi escolhido {opcao_moradia} e o orçamento é de R${orcamento:.2f}.")
 
-    print(f"Foi escolhido {opcao_moradia} e o orçamento é {orcamento}.")
+        else:
+            print("Opção não encontrada, escolha novamente!")
+    except:
+        print("Opção de moradia não identificada! Tente novamente!")
+
+contrato = 2000
 
 while True:
-    opcao_moradia = ""
     valor_orcamento()
     input("Pressione qualquer tecla para reiniciar!")
