d4cb4152786e27c9b354dcf6fc4935cb01f6bcb2
Priscylla Pfleger <cyllapfleger@hotmail.com>
2026-02-14 18:10:44 -0300

    Atualizado com orçamento finalizado, falta arquivo csv

--- imobiliaria.py
+++ imobiliaria.py
@@ -1,11 +1,14 @@
+import os
+import csv
+
 def quartos(opcao_moradia, qtdade_quartos):
-    if opcao_moradia == "apartamento" and qtdade_quartos == 1:
+    if opcao_moradia == "apartamento" and qtdade_quartos == "1":
         return  700
-    elif opcao_moradia == "apartamento" and qtdade_quartos == 2:
+    elif opcao_moradia == "apartamento" and qtdade_quartos == "2":
         return 700 + 200
-    elif opcao_moradia == "casa" and qtdade_quartos == 1:
+    elif opcao_moradia == "casa" and qtdade_quartos == "1":
         return 900
-    elif opcao_moradia == "casa" and qtdade_quartos == 2:
+    elif opcao_moradia == "casa" and qtdade_quartos == "2":
         return 900 + 250
     else:
         print("Erro no processamento!")
@@ -30,9 +33,9 @@ def garagem_estudio(orcamento, garagem):
     if garagem == "S":
         orcamento += 250
         quer_vaga_extra = input("Deseja adicionar vaga(s) extra(s)? (S) sim ou (N) não ").upper()
-        orcamento = vaga_extra_garagem(orcamento, quer_vaga_extra)
+        orcamento, vaga_extra = vaga_extra_garagem(orcamento, quer_vaga_extra)
+        return orcamento, vaga_extra
     else:
-        print("Não identificamos sua escolha!")
         return orcamento
 
 def vaga_extra_garagem(orcamento, quer_vaga_extra):
@@ -41,62 +44,89 @@ def vaga_extra_garagem(orcamento, quer_vaga_extra):
         quer_vaga_extra = input("Digite novamente! Você deseja vaga(s) extra(s) de garagem? (S)sim ou (N)não ").upper()
 
     if quer_vaga_extra == "S":
-        vaga_extra = int(input("Quantas vagas extras? "))
-        try:
-            if vaga_extra > 0:
-                return orcamento + (vaga_extra * 60)
-            else:
-                print("Erro no processamento!")
-        except ValueError:
-            print("Valor inválido! Nenhuma vaga extra adicionada.")
-            return orcamento
+        while True:
+            try: 
+                vaga_extra = int(input("Quantas vagas extras? "))
+                       
+                if vaga_extra > 0:
+                    return orcamento + (vaga_extra * 60), quer_vaga_extra
+                elif vaga_extra == 0:
+                    print("Nenhuma vaga extra será adicionada.")
+                    return orcamento, quer_vaga_extra
+                else:
+                    print("Não identificamos sua escolha! Número não pode ser negativo.")
+                    continue
+            except ValueError:
+                print("Entrada inválida! Digite apenas números.")
+    else:
+        return orcamento, quer_vaga_extra
 
-def desconto_crianca(orcamento):
+def desconto_sem_crianca(orcamento):
     crianca = input("Você tem criança(s)? (S) sim ou (N) não ").upper()
-    
-    try:
-        if crianca == "N":
-            return orcamento * 0.95
-        elif crianca == "S":
-            return orcamento
-        else:
-            print("Erro no processamento, digite novamente!")
-    except:
-        print("Informação de crianças não identificada! Tente novamente!")
+    while crianca != "S" and crianca != "N":
+        print("Não identificamos sua escolha!")
+        crianca = input("Digite novamente! Tem criança(s)? (S)sim ou (N)não ").upper()
+
+    if crianca == "N":
+        return orcamento * 0.95, crianca
+    else:
+        return orcamento, crianca
 
 def valor_orcamento():
     orcamento = 0
-    opcao_moradia = input("Você quer apartamento, casa ou estudio? ")
-
+    limpar_tela()
+    print("__________ TIPO DE MORADIA __________\n")
+    opcao_moradia = input("Você quer apartamento, casa ou estudio? ").lower()
     try:        
         if opcao_moradia == "apartamento" or opcao_moradia == "casa":
-            qtdade_quartos = int(input("Digite a quantidade de quartos desejada (1 ou 2): "))
-            try:
-                while qtdade_quartos < 1 or qtdade_quartos > 2:
-                    print("Quantidade de quartos inválida!")
-                    qtdade_quartos = int(input("Digite novamente a quantidade de quartos desejada: "))
-            except: exit()
+            limpar_tela()
+            qtdade_quartos = input("Digite a quantidade de quartos desejada (1 ou 2): ")
+            while qtdade_quartos != "1" and qtdade_quartos != "2":
+                print("Quantidade de quartos inválida!")
+                qtdade_quartos = input("Digite novamente a quantidade de quartos desejada: ")
+            limpar_tela()
             garagem = input("Você deseja uma vaga de garagem? (S)sim ou (N)não ").upper()
             orcamento = quartos(opcao_moradia, qtdade_quartos)
             orcamento = garagem_geral(orcamento, garagem)
-            
-            orcamento = desconto_crianca(orcamento)
-            print(f"Foi escolhido {opcao_moradia} e o orçamento é de R${orcamento:.2f}.")
+            limpar_tela()
+            orcamento, crianca = desconto_sem_crianca(orcamento)
+            print(f"Foi escolhido:\nmoradia - {opcao_moradia}\nquantidade de quartos - {qtdade_quartos}\ngaragem - {garagem}\ncriança - {crianca}\norçamento sem valor de contrato - R${orcamento:.2f}.")
+            input ("\nAperte 'ENTER' para voltar!")
 
         elif opcao_moradia == "estudio":
             orcamento = 1200
+            limpar_tela()
             garagem = input("Você deseja uma vaga de garagem? (S)sim ou (N)não ").upper()
-            orcamento = garagem_estudio(orcamento, garagem)
-            orcamento = desconto_crianca(orcamento)
-            print(f"Foi escolhido {opcao_moradia} e o orçamento é de R${orcamento:.2f}.")
-
+            orcamento, quer_vaga_extra = garagem_estudio(orcamento, garagem)
+            limpar_tela()
+            orcamento, crianca = desconto_sem_crianca(orcamento)
+            print(f"Foi escolhido:\nmoradia - {opcao_moradia}\nquantidade de quartos - {qtdade_quartos}\ngaragem - {garagem}\nvaga extra - {quer_vaga_extra}\ncriança - {crianca}\norçamento sem valor de contrato - R${orcamento:.2f}.")
+            input ("Aperte 'ENTER' para voltar!")
+            
         else:
-            print("Opção não encontrada, escolha novamente!")
+            print("Opção não encontrada")
+            input ("Aperte 'ENTER' para voltar!")
     except:
-        print("Opção de moradia não identificada! Tente novamente!")
+        print("Opção de moradia não identificada!")
+        input ("Aperte 'ENTER' para voltar!")
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def encerrar_sistema():
+    input ("Aperte 'ENTER' para encerrar!")
 
 contrato = 2000
 
 while True:
-    valor_orcamento()
-    input("Pressione qualquer tecla para reiniciar!")
+    limpar_tela()
+    print("---------- IMOBILIÁRIA PFLEGER ----------\n")
+    quer_orcamento = input("Gostaria de fazer um orçamento? (S) sim ou (N) não ").upper()
+    if quer_orcamento == "S":
+        valor_orcamento()
+        
+    elif quer_orcamento == "N":
+        encerrar_sistema()
+        break
+    else:
+        input("Não entendi! Aperte 'ENTER' para voltar!")
